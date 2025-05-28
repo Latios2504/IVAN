031bb1bf93ce1b8e9042989d8596fbd87ba38055
DungBui7203 <dungml10a1@gmail.com>
2025-05-28 19:11:11 +0700

    Add Service folder

--- back-end/BE_IVanWebApi/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ back-end/BE_IVanWebApi/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bda00cdf07ca1a074d4b3e8e566c26f0c9269a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19499bde72787ed48f3cc454d2a9c4e0ef59cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
