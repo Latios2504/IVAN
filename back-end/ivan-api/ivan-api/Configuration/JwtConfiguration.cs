16a2192613f92316377c1cb3288311ea8e10f493
NTDungSE <132890966+NTDungSE@users.noreply.github.com>
2025-06-09 22:59:43 +0700

    Delete back-end/ivan-api directory

--- back-end/ivan-api/ivan-api/Configuration/JwtConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace ivan_api.Configuration;
-
-public class JwtConfiguration
-{
-    public string SecretKey { get; set; } = string.Empty;
-    public string Issuer { get; set; } = string.Empty;
-    public string Audience { get; set; } = string.Empty;
-    public int ExpiryMinutes { get; set; } = 60;
-}
--- back-end/ivan-api/ivan-api/Services/IPasswordHashingService.cs
+++
@@ -1,8 +0,0 @@
-namespace ivan_api.Services;
-
-public interface IPasswordHashingService
-{
-    string GenerateSalt();
-    string HashPassword(string password, string salt);
-    bool VerifyPassword(string password, string hash, string salt);
-}
